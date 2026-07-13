4065f0fd9ace77ddf15adfe4fc35fc3e0cceec9e
neverland <chenjiahan.jait@bytedance.com>
2026-07-13 13:56:29 +0800

    chore: remove test utility re-exports

--- test/basic/rsbuild.config.ts
+++ test/basic/rsbuild.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginUmd } from '../../dist';
-import { getRandomPort } from '../helper';
+import { getRandomPort } from '@rstackjs/test-utils';
 
 export default defineConfig({
   plugins: [
--- test/split-chunks/rsbuild.config.ts
+++ test/split-chunks/rsbuild.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginUmd } from '../../dist';
-import { getRandomPort } from '../helper';
+import { getRandomPort } from '@rstackjs/test-utils';
 
 export default defineConfig({
   plugins: [
